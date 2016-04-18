804556627e376dc56addb2bcd89d5c0d079d95e3
Adam Tornhill <adam@adamtornhill.com>
2016-04-18 19:11:42 +0200

    Add an API to the stop watch include file

--- 2_State/StatePattern/DigitalStopWatch.h
+++ 2_State/StatePattern/DigitalStopWatch.h
@@ -6,4 +6,16 @@ A pointer to an incomplete type (hides the implementation details).
 */
 typedef struct DigitalStopWatch* DigitalStopWatchPtr;
 
+/*
+A watch is implemented using the First-Class ADT pattern.
+*/
+
+DigitalStopWatchPtr createWatch(void);
+
+void destroyWatch(DigitalStopWatchPtr instance);
+
+void startWatch(DigitalStopWatchPtr instance);
+
+void stopWatch(DigitalStopWatchPtr instance);
+
 #endif
